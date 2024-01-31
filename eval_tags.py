06aa6865557a5c8f31ba973a55f256a89da4e3fb
Karla Hernandez <karlahrnndz.dev@gmail.com>
2024-01-31 02:35:06 -0800

    removed space from labels.csv input, output still has space.

--- eval_tags.py
+++ eval_tags.py
@@ -1,8 +1,14 @@
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from transformers import pipeline
+
 import pandas as pd
-import logging
 import os
+
+# ----------------------------------------------------------------------------------- #
+#                                  Define constants                                   #
+# ----------------------------------------------------------------------------------- #
+
+TAGGED_ACC_FILEPATH = os.path.join('data', 'output', 'tagged_accidents.csv')
+TAGGED_ACC_EXP_FILEPATH = os.path.join('data', 'output', 'tagged_accidents_exp.csv')
+ACCIDENT_FILEPATH = os.path.join('data', 'output', 'accident_reports.csv')
 
 
 # ----------------------------------------------------------------------------------- #
@@ -12,16 +18,32 @@ import os
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-TAGGED_ACC_FILEPATH = os.path.join('data', 'output', 'tagged_accidents.csv')
-ACCIDENT_FILEPATH = os.path.join('data', 'output', 'accident_reports.csv')
+
+# ----------------------------------------------------------------------------------- #
+#                                    Analyze Tags                                     #
+# ----------------------------------------------------------------------------------- #
+
 
 # Load accident descriptions
 tagged_df = pd.read_csv(TAGGED_ACC_FILEPATH)
 
+# Explode tags
 tagged_df['tags'] = tagged_df['tags'].apply(eval)
 tagged_df = tagged_df.explode('tags', ignore_index=True)
 
-# acc_df = pd.read_csv(ACCIDENT_FILEPATH)
-# tagged_df = tagged_df.merge(acc_df, how='left', on='acc_id')
+# Add accident information by acc_id
+acc_df = pd.read_csv(ACCIDENT_FILEPATH)
+tagged_df = tagged_df.merge(acc_df, how='left', on='acc_id')
+
+# Splitting the 'tags' column into two separate columns
+tagged_df[['tag', 'prob']] = tagged_df['tags'].apply(pd.Series)
+tagged_df.drop(columns=['tags'], inplace=True)
+
+# Save exploded dataframe
+tagged_df.to_csv(TAGGED_ACC_EXP_FILEPATH, index=False)
+
+# Count number of occurrences per tag and probability  # TODO – below just for fun
+df = tagged_df.loc[tagged_df.prob.ge(0.8), :].copy()
+print(df.loc[df.tag.eq('extreme cold') & df.tag.ne('frostbite') & df.tag.ne('hypothermia'), :].head())
 
 print('hi')
